4a8e1574bb261f3d1384229716fbefb503d55cab
Victor Marian Jurj <victor.jurj@yahoo.com>
2025-02-11 12:51:34 +0200

    adjust index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import BooksList from "./pages/book/BooksList";
+import CreateBook from "./pages/book/CreateBook";
+import ViewBook from "./pages/book/ViewBook";
+import EditBook from "./pages/book/EditBook";
+import { Provider } from "react-redux";
+import store from "./stores/store";
+import ViewBookUser from "./pages/user/User_UI/ViewBookUser";
+import BooksListUser from "./pages/user/User_UI/BooksListUser";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/biblioteca.admin/books", element: <BooksList /> },
+      { path: "/biblioteca.admin/books/create", element: <CreateBook /> },
+      { path: "/biblioteca.admin/books/:bookId", element: <ViewBook /> },
+      { path: "/biblioteca.admin/books/:bookId/edit", element: <EditBook /> },
+      /////////////user////
+      { path: "/biblioteca/books", element: <BooksListUser /> },
+      { path: "/biblioteca/books/:bookId", element: <ViewBookUser /> },
+    ],
+  },
+]);
+
+//JSX
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
