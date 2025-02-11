b64e86e0ad3300c1ab6c7bf4f7af26673044bfce
Victor Marian Jurj <victor.jurj@yahoo.com>
2025-02-11 12:22:40 +0200

    create app module

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AdminLayout from "./layouts/AdminLayout";
+
+function App() {
+  return <AdminLayout />;
+}
+
+// import UserLayout from "./pages/user/layouts/UserLayout";
+
+// function App() {
+//   return <UserLayout />;
+// }
+
+export default App;
