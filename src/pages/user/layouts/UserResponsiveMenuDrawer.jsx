9438f6b0d1d760881f055419272a1fdc3e9e4333
Victor Marian Jurj <victor.jurj@yahoo.com>
2025-02-14 12:30:23 +0200

    add login button and functionality

--- src/pages/user/layouts/UserResponsiveMenuDrawer.jsx
+++ src/pages/user/layouts/UserResponsiveMenuDrawer.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -9,6 +10,23 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import UserAvatar from "../layouts/UserAvatar";
 import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 import { Link } from "react-router-dom";
+import AdminLogin from "../../book/AdminLogin";
+import Button from "@mui/material/Button"; // Import Material UI Button
+import { styled } from "@mui/system";
+
+// Styled Box for the login button section
+const LoginBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: "20px",
+  padding: "10px",
+  backgroundColor: "transparent", // Remove background color
+  borderRadius: "8px",
+  boxShadow: "none", // Remove shadow for the box
+  width: "90%",
+});
 
 const ResponsiveMenuDrawer = () => {
   const menuItems = [
@@ -19,9 +37,11 @@ const ResponsiveMenuDrawer = () => {
     },
   ];
 
-  const accountMenuItems = [
-    // { text: "My account", icon: AccountBoxIcon, to: "/account" },
-  ];
+  const [showAdminLogin, setShowAdminLogin] = useState(false);
+
+  const handleAdminLoginClick = () => {
+    setShowAdminLogin(true); // Show the admin login form
+  };
 
   return (
     <div>
@@ -49,22 +69,36 @@ const ResponsiveMenuDrawer = () => {
         ))}
       </List>
       <Divider />
-      <List sx={{ padding: 0 }}>
-        {accountMenuItems.map(({ text, icon: ItemIcon, to }) => (
-          <ListItem key={text} disablePadding sx={{ marginBottom: 0 }}>
-            <ListItemButton>
-              <ListItemIcon>
-                <ItemIcon />
-              </ListItemIcon>
-              <Link to={to} style={{ textDecoration: "none" }}>
-                <ListItemText primary={text} />
-              </Link>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
 
-      <Divider />
+      {/* Login Box with Material UI Button */}
+      <LoginBox>
+        {!showAdminLogin ? (
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleAdminLoginClick}
+            sx={{
+              width: "150px", // Set button width
+              borderRadius: "8px", // Rounded corners
+              boxShadow: "none", // Remove shadow for button
+              backgroundColor: "#1C7343", // Set the desired background color
+              color: "white", // Set the text color to white
+              border: "1px solid #1C7343", // Add a border to match the background color
+              "&:hover": {
+                backgroundColor: "#155e36", // Darker green on hover
+                borderColor: "#155e36", // Darker border color on hover
+              },
+            }}
+          >
+            Admin Login
+          </Button>
+        ) : (
+          <AdminLogin />
+        )}
+      </LoginBox>
+
+      {/* <Divider /> */}
       <Box sx={{ mt: "60px" }}>
         <img
           src="/images/logo.jpg"
